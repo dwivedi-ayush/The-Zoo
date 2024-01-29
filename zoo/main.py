94ea724c9fbaab9c7b23e89fd2a2e140a810e347
dwivedi-ayush <akshatayu@gmail.com>
2024-01-29 15:19:40 +0530

    initial test

--- zoo/main.py
+++ zoo/main.py
@@ -5,21 +5,11 @@ import os
 
 load_dotenv(find_dotenv())
 
-ai.api_key = os.getenv("API_KEY")
-client = ai.OpenAI()
+api_key = os.getenv("API_KEY")
+client = ai.OpenAI(api_key=api_key)
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {
-            "role": "system",
-            "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
-        },
-        {
-            "role": "user",
-            "content": "Compose a poem that explains the concept of recursion in programming.",
-        },
-    ],
+response = client.completions.create(
+    model="gpt-3.5-turbo-instruct", prompt="hi, who are you?", temperature=0
 )
 
-print(completion.choices[0].message)
+print(response.choices[0].text)
