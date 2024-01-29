cbd7d71d0bf5b281e3f1731c7af05faee6f4a3d3
dwivedi-ayush <akshatayu@gmail.com>
2024-01-29 15:30:33 +0530

    testing

--- zoo/main.py
+++ zoo/main.py
@@ -8,8 +8,20 @@ load_dotenv(find_dotenv())
 api_key = os.getenv("API_KEY")
 client = ai.OpenAI(api_key=api_key)
 
-response = client.completions.create(
-    model="gpt-3.5-turbo-instruct", prompt="hi, who are you?", temperature=0
-)
 
+""" 
+check client.chat.completion also instead of prompt it has 
+messages(array of previous chat like messages) and role(syste, user, assistant and function), 
+may be more suitable for our needs
+model predicts the next logical step
+this is legacy (used here for testing only)
+"""
+
+response = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt="hi, who are you?",
+    temperature=0,
+    max_tokens=10,
+)
+# if max topen very less , reason for stopping becomes length
 print(response.choices[0].text)
