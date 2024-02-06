93b7288b955a3e240d917bc370e58be59720349b
Arnish-B <arnish.2020@vitstudent.ac.in>
2024-02-06 13:44:59 +0530

    explore_tweets with 10 latest tweets + json server

---
+++ zoo/explore.py
@@ -0,0 +1,26 @@
+import requests
+from datetime import datetime
+
+def explore_tweets():
+    response = requests.get('http://localhost:3001/tweets')
+    if response.status_code == 200:
+        tweets = response.json()
+        descriptions = {}
+        if len(tweets) > 10:
+            sorted_tweets = sorted(tweets, key=lambda x: datetime.fromisoformat(
+                x['createdAt']), reverse=True)
+            latest_tweets = sorted_tweets[:10]
+            for tweet in latest_tweets:
+                descriptions[tweet['id']] = tweet['description']
+                # print(tweet)
+        else:
+            for tweet in tweets:
+                descriptions[tweet['id']] = tweet['description']
+                # print(tweet)
+    else:
+        print(f"Request failed with status code {response.status_code}")
+    return descriptions
+
+
+tweet_list = explore_tweets()
+print(tweet_list)
