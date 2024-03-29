7c51d688a77bec58659159de9e23db805b4ff01d
Arnish-B <arnish.2020@vitstudent.ac.in>
2024-03-29 20:52:05 +0530

    SOME CODE MORE

--- zoo/db.py
+++ zoo/db.py
@@ -1,23 +1,26 @@
 from dotenv import dotenv_values
 from pymongo import MongoClient
 from datetime import datetime
+from bson.objectid import ObjectId
 
-config = dotenv_values(".env")
-mongodb_client = MongoClient(config["ATLAS_URI"])
 
-database = mongodb_client[config["DB_NAME"]]
-tweets_collection = database['tweets']
-print("Connected to the MongoDB database!")
+# config = dotenv_values(".env")
+# mongodb_client = MongoClient(config["ATLAS_URI"])
+
+# database = mongodb_client[config["DB_NAME"]]
+# tweets_collection = database['tweets']
 
 def save_reply(personality_id, tweet_id, description):
     config = dotenv_values(".env")
     mongodb_client = MongoClient(config["ATLAS_URI"])
 
     database = mongodb_client[config["DB_NAME"]]
+    replies_collection = database['replies']
     tweets_collection = database['tweets']
     print("Connected to the MongoDB database!")
     # Retrieve the tweet having tweet_id from the tweets collection
-    tweet = tweets_collection.find_one({"_id": tweet_id})
+    tweet = tweets_collection.find_one({"_id": ObjectId(tweet_id)})
+    
     
     new_reply = {
         "alias": personality_id,
@@ -27,23 +30,22 @@ def save_reply(personality_id, tweet_id, description):
     # If "reply" string inside the retrieved tweet object is empty
     if not tweet["replies"]:
         # create a new array in the "replies" collection
-        result = tweets_collection.update_one(
-            {"_id": tweet_id},
+        result = replies_collection.insert_one(
+            {"_id": ObjectId(tweet_id)},
             {"$set": {"replies": [new_reply]}},
         )
     else:
         # append the reply into the array Retrieved using the reply_id ( The reply string )
-        result = tweets_collection.update_one(
-            {"_id": tweet_id},
+        result = replies_collection.update_one(
+            {"_id": ObjectId(tweet_id)},
             {"$push": {"replies": new_reply}},
         )
-    mongodb_client.close()
-    print("Connection to the MongoDB database closed!")
+    print(result)
     if result.acknowledged:
         print("Reply saved successfully")
         # Update the replies field of the tweet schema and set it to the unique ID generated
         tweets_collection.update_one(
-            {"_id": tweet_id},
+            {"_id": ObjectId(tweet_id)},
             {"$set": {"replies": result.inserted_id}},
         )
         return True
@@ -51,33 +53,28 @@ def save_reply(personality_id, tweet_id, description):
         print("Failed to save reply")
         return False
 
-def save_tweet(personality_id, tweet):
-    config = dotenv_values(".env")
-    mongodb_client = MongoClient(config["ATLAS_URI"])
+# def save_tweet(personality_id, tweet):
+#     config = dotenv_values(".env")
+#     mongodb_client = MongoClient(config["ATLAS_URI"])
 
-    database = mongodb_client[config["DB_NAME"]]
-    tweets_collection = database['tweets']
-    print("Connected to the MongoDB database!")
-    new_tweet = {
-        "userId": personality_id,
-        "description": tweet,
-        "likes": [],
-        "replies": "",
-        "createdAt": datetime.now(),
-        "updatedAt": datetime.now()
-    }
-    result = tweets_collection.insert_one(new_tweet)
-    mongodb_client.close()
-    print("Connection to the MongoDB database closed!")
-    if result.acknowledged:
-        print("Tweet saved successfully")
+#     database = mongodb_client[config["DB_NAME"]]
+#     tweets_collection = database['tweets']
+#     print("Connected to the MongoDB database!")
+#     new_tweet = {
+#         "userId": personality_id,
+#         "description": tweet,
+#         "likes": [],
+#         "replies": "",
+#         "createdAt": datetime.now(),
+#         "updatedAt": datetime.now()
+#     }
+#     result = tweets_collection.insert_one(new_tweet)
+#     if result.acknowledged:
+#         print("Tweet saved successfully")
         
-        return True
-    else:
-        print("Failed to save tweet")
-        return False
-
-mongodb_client.close()
-print("Connection to the MongoDB database closed!")
+#         return True
+#     else:
+#         print("Failed to save tweet")
+#         return False
 
 
