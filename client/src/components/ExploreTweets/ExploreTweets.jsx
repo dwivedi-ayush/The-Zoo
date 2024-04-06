a10895e14572305b97c6cac890ec5ced060e3825
dwivedi-ayush <akshatayu@gmail.com>
2024-04-06 23:45:48 +0530

    improved frontend

--- client/src/components/ExploreTweets/ExploreTweets.jsx
+++ client/src/components/ExploreTweets/ExploreTweets.jsx
@@ -1,28 +1,31 @@
-import React, { useState,useRef,useCallback } from "react";
-
+import React, { useState, useRef, useCallback } from "react";
+import { List } from "react-content-loader";
 // import axios from "axios";
 // import { useSelector } from "react-redux";
 import Tweet from "../Tweet/Tweet";
 import useExploreTweet from "../../useExploreTweet";
 
 const ExploreTweets = () => {
-  const[pageNumber,setPageNumber]=useState(1);
-
-  const {loading,error,tweets,hasMore}=useExploreTweet(pageNumber)
+  const [pageNumber, setPageNumber] = useState(1);
+  const items = new Array(2).fill(null);
+  const { loading, error, tweets, hasMore } = useExploreTweet(pageNumber);
 
   const [explore, setExplore] = useState(null);
   // const { currentUser } = useSelector((state) => state.user);
-  const observer =useRef();
-  const lastTweetElementRef =  useCallback(node =>{
-    if(loading) return
-    if(observer.current) observer.current.disconnect()
-    observer.current = new IntersectionObserver(entries =>{
-      if(entries[0].isIntersecting && hasMore){
-        setPageNumber(prevPageNumber=>prevPageNumber+1)
-      }
-    })
-    if (node) observer.current.observe(node)
-  },[loading,hasMore])
+  const observer = useRef();
+  const lastTweetElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
   // useEffect(() => {
   //   const fetchData = async () => {
   //     try {
@@ -36,22 +39,45 @@ const ExploreTweets = () => {
   // }, [currentUser._id]);
   return (
     <div className="mt-6">
-      {loading && <h2>Loading...</h2>}
-      {error && <h2>Error</h2>}
-      {tweets &&
-        tweets.map((tweet,index) => {
-          if(tweets.length === index+1){
-            return (<div ref={lastTweetElementRef} key={tweet._id} className="p-2">
-                        <Tweet tweet={tweet} setData={setExplore} />
-                        
-                    </div>)
-          }
-          else{
-            return (
-            <div key={tweet._id} className="p-2">
-              <Tweet tweet={tweet} setData={setExplore} />
+      {!error &&
+        loading &&
+        items.map(() => (
+          <>
+            <div className="border-b-2 pb-6 pt-6">
+              <List />
             </div>
-          )};
+          </>
+        ))}
+      {error && (
+        <>
+          <div
+            class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+            role="alert"
+          >
+            <p class="font-bold">Fetch Error</p>
+            <p>Cannot retrieve Tweets..</p>
+          </div>
+        </>
+      )}
+      {tweets &&
+        tweets.map((tweet, index) => {
+          if (tweets.length === index + 1) {
+            return (
+              <div>
+                <div ref={lastTweetElementRef} key={tweet._id} className="p-2">
+                  <Tweet tweet={tweet} setData={setExplore} />
+                </div>
+              </div>
+            );
+          } else {
+            return (
+              <div>
+                <div key={tweet._id} className="p-2">
+                  <Tweet tweet={tweet} setData={setExplore} />
+                </div>
+              </div>
+            );
+          }
         })}
     </div>
   );
